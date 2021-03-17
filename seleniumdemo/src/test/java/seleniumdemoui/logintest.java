509590aaaeea443bb479312a10fa896df3910ba0
MAHAKAL <MAHAKAL@DESKTOP-HV8MD9Q>
2021-03-18 00:33:13 +0530

    notifications handles

--- seleniumdemo/src/test/java/seleniumdemoui/logintest.java
+++ seleniumdemo/src/test/java/seleniumdemoui/logintest.java
@@ -2,16 +2,20 @@ package seleniumdemoui;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class logintest {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
+		
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
 		
 		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver();
+		ChromeDriver driver = new ChromeDriver(options);
 		
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
@@ -19,9 +23,7 @@ public class logintest {
 		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("yoyojitu19@gmail.com");
 		driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("jitendra@123");
 driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")).click();
-Thread.sleep(500);
 
-driver.switchTo().alert().dismiss();
-
+driver.quit();
 	}
 }
