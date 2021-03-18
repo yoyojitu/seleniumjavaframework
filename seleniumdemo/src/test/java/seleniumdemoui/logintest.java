99772aa872ccb0aeb776007981ac39830b3096b6
MAHAKAL <MAHAKAL@DESKTOP-HV8MD9Q>
2021-03-19 01:07:46 +0530

    upds

--- seleniumdemo/src/test/java/seleniumdemoui/logintest.java
+++ seleniumdemo/src/test/java/seleniumdemoui/logintest.java
@@ -18,12 +18,13 @@ public class logintest {
 		ChromeDriver driver = new ChromeDriver(options);
 		
 		driver.get("https://www.facebook.com/");
+		Thread.sleep(500);
 		driver.manage().window().maximize();
 		Thread.sleep(500);
 		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("yoyojitu19@gmail.com");
 		driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("jitendra@123");
 driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")).click();
-Thread.sleep(1000);
+Thread.sleep(10000);
 driver.quit();
 	}
 }
