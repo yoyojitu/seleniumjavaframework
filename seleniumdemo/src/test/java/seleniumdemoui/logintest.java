303c44d34f9c84efc54643264bb9af7637efd49c
MAHAKAL <MAHAKAL@DESKTOP-HV8MD9Q>
2021-03-18 21:51:41 +0530

    micro

--- seleniumdemo/src/test/java/seleniumdemoui/logintest.java
+++ seleniumdemo/src/test/java/seleniumdemoui/logintest.java
@@ -23,7 +23,7 @@ public class logintest {
 		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("yoyojitu19@gmail.com");
 		driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("jitendra@123");
 driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")).click();
-
+Thread.sleep(1000);
 driver.quit();
 	}
 }
